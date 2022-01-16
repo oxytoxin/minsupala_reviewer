17a3e47c7b5e4775ef270cd148f948e0169e5758
oxytoxin <oxytoxinsgrace@gmail.com>
2022-01-16 23:54:38 +0800

    import json directly

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,7 +1,5 @@
-import fs from 'fs';
+import items from '$lib/items.json';
 export async function get() {
-	let file = fs.readFileSync('src/lib/items.json');
-	let items = JSON.parse(file);
 	return {
 		body: items
 	};
